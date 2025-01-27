e3411d661f8a0b32d10d444640e3a5143465e93a
Ayush Chugh <hey@ayushchugh.com>
2025-01-27 11:36:24 +0530

    feat: add PostgreSQL configuration and health check endpoint

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,14 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 
-const db = drizzle(process.env.DATABASE_URL!);
+const db = drizzle({
+	connection: {
+		user: process.env.DB_POSTGRES_USER!,
+		password: process.env.DB_POSTGRES_PASSWORD!,
+		database: process.env.DB_POSTGRES_DB!,
+		host: process.env.DB_POSTGRES_HOST!,
+		port: Number(process.env.DB_POSTGRES_PORT!),
+		ssl: false,
+	},
+});
+
+export default db;
--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -1,8 +1,27 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
+import db from "../../db";
+import { sql } from "drizzle-orm";
 
-export function getHeathHandler(req: Request, res: Response) {
-	res.status(StatusCodes.OK).json({
-		message: "Server is running",
-	});
+export async function getHeathHandler(req: Request, res: Response) {
+	try {
+		const status = await db.execute(sql`select 1`);
+
+		if (!status) {
+			return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+				message: "Database is not connected",
+				status: StatusCodes.INTERNAL_SERVER_ERROR,
+			});
+		}
+
+		return res.status(StatusCodes.OK).json({
+			status: StatusCodes.OK,
+			message: "Server is running",
+		});
+	} catch (err) {
+		return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+			message: "Internal server error",
+			status: StatusCodes.INTERNAL_SERVER_ERROR,
+		});
+	}
 }
