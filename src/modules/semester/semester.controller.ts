ad9d23a4216212f4e8715d499687efcf7e37aa5a
Ayush Chugh <hey@ayushchugh.com>
2025-03-10 14:12:35 +0530

    feat: get all semesters

--- src/modules/semester/semester.controller.ts
+++ src/modules/semester/semester.controller.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import { PostCreateSemesterBody } from "./semester.schema";
 import db from "@/db";
 import { semesterTable } from "@/db/schema/semester";
+import { isNull } from "drizzle-orm";
 
 export async function postCreateSemesterHandler(
   req: Request<{}, {}, PostCreateSemesterBody>,
@@ -24,6 +25,37 @@ export async function postCreateSemesterHandler(
 
     return;
   } catch (err) {
+    console.error(err);
+
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: "Internal Server Error",
+    });
+
+    return;
+  }
+}
+
+export async function getAllSemesterHandler(req: Request, res: Response) {
+  try {
+    const semesters = await db
+      .select({
+        id: semesterTable.id,
+        title: semesterTable.title,
+        start_date: semesterTable.start_date,
+        end_date: semesterTable.end_date,
+      })
+      .from(semesterTable)
+      .where(isNull(semesterTable.deleted_at));
+
+    res.status(StatusCodes.OK).json({
+      message: "semesters fetched successfully",
+      payload: semesters,
+    });
+
+    return;
+  } catch (err) {
+    console.error(err);
+
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       message: "Internal Server Error",
     });
--- src/modules/semester/semester.routes.ts
+++ src/modules/semester/semester.routes.ts
@@ -3,15 +3,20 @@ import { requireRoleMiddleware } from "@/middlewares/require-role.middleware";
 import { validateRequestBody } from "@/middlewares/validate-request.middleware";
 import { Router } from "express";
 import { postCreateSemesterSchema } from "./semester.schema";
-import { postCreateSemesterHandler } from "./semester.controller";
+import {
+  getAllSemesterHandler,
+  postCreateSemesterHandler,
+} from "./semester.controller";
 
 const semesterRouter = Router();
 
-semesterRouter.post(
-  "/",
-  requireRoleMiddleware(Role.ADMIN),
-  validateRequestBody(postCreateSemesterSchema),
-  postCreateSemesterHandler,
-);
+semesterRouter
+  .route("/")
+  .post(
+    requireRoleMiddleware(Role.ADMIN),
+    validateRequestBody(postCreateSemesterSchema),
+    postCreateSemesterHandler,
+  )
+  .get(getAllSemesterHandler);
 
 export default semesterRouter;
