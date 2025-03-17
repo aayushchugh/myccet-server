56beb93e51984c943ea6cc83bf290d369c35d7b9
Ayush Chugh <hey@ayushchugh.com>
2025-03-17 10:08:16 +0530

    fix: update faculty without phone

--- src/modules/admin/admin.schema.ts
+++ src/modules/admin/admin.schema.ts
@@ -3,48 +3,52 @@ import { Designation } from "../../db/schema/user";
 
 const instituteDomain = process.env.INSTITUTE_DOMAIN;
 if (!instituteDomain) {
-  throw new Error(
-    "INSTITUTE_DOMAIN is not defined in the environment variables.",
-  );
+	throw new Error(
+		"INSTITUTE_DOMAIN is not defined in the environment variables."
+	);
 }
 
 const emailRegex = new RegExp(
-  `^[a-zA-Z0-9._%+-]+@${instituteDomain.replace(/\./g, "\\.")}$`,
+	`^[a-zA-Z0-9._%+-]+@${instituteDomain.replace(/\./g, "\\.")}$`
 );
 
 export const postAdminBodySchema = z.object({
-  email: z
-    .string()
-    .email({ message: "please enter a valid email" })
-    .regex(emailRegex, `email must be on ${instituteDomain} domain`),
-  password: z
-    .string({ required_error: "password is required" })
-    .min(6, { message: "password should be at least 6 characters" }),
-  first_name: z.string({ required_error: "first name is required" }),
-  middle_name: z.string().optional(),
-  last_name: z.string().optional(),
-  phone: z
-    .number({ required_error: "phone number is required" })
-    .min(1000000000, { message: "phone number should be at least 10 digits" })
-    .max(9999999999, { message: "phone number should be at least 10 digits" }),
-  designation: z.enum([Designation.Principal, Designation.Maintenance]),
+	email: z
+		.string()
+		.email({ message: "please enter a valid email" })
+		.regex(emailRegex, `email must be on ${instituteDomain} domain`),
+	password: z
+		.string({ required_error: "password is required" })
+		.min(6, { message: "password should be at least 6 characters" }),
+	first_name: z.string({ required_error: "first name is required" }),
+	middle_name: z.string().optional(),
+	last_name: z.string().optional(),
+	phone: z
+		.number({ required_error: "phone number is required" })
+		.min(1000000000, { message: "phone number should be at least 10 digits" })
+		.max(9999999999, { message: "phone number should be at least 10 digits" }),
+	designation: z.enum([Designation.Principal, Designation.Maintenance]),
 });
 
 export type PostAdminBody = z.infer<typeof postAdminBodySchema>;
 
 export const putAdminBodySchema = z.object({
-  email: z.string().email({ message: "please enter a valid email" }).optional(),
-  first_name: z.string().optional(),
-  middle_name: z.string().optional(),
-  last_name: z.string().optional(),
-  phone: z
-    .number()
-    .min(1000000000, { message: "phone number should be at least 10 digits" })
-    .max(9999999999, { message: "phone number should be at least 10 digits" })
-    .optional(),
-  designation: z
-    .enum([Designation.Principal, Designation.Maintenance])
-    .optional(),
+	email: z
+		.string()
+		.email({ message: "please enter a valid email" })
+		.regex(emailRegex, `email must be on ${instituteDomain} domain`)
+		.optional(),
+	first_name: z.string().optional(),
+	middle_name: z.string().optional(),
+	last_name: z.string().optional(),
+	phone: z
+		.number()
+		.min(1000000000, { message: "phone number should be at least 10 digits" })
+		.max(9999999999, { message: "phone number should be at least 10 digits" })
+		.optional(),
+	designation: z
+		.enum([Designation.Principal, Designation.Maintenance])
+		.optional(),
 });
 
 export type PutAdminBody = z.infer<typeof putAdminBodySchema>;
--- src/modules/faculty/faculty.schema.ts
+++ src/modules/faculty/faculty.schema.ts
@@ -3,44 +3,52 @@ import { Designation } from "../../db/schema/user";
 
 const instituteDomain = process.env.INSTITUTE_DOMAIN;
 if (!instituteDomain) {
-  throw new Error(
-    "INSTITUTE_DOMAIN is not defined in the environment variables."
-  );
+	throw new Error(
+		"INSTITUTE_DOMAIN is not defined in the environment variables."
+	);
 }
 
 const emailRegex = new RegExp(
-  `^[a-zA-Z0-9._%+-]+@${instituteDomain.replace(/\./g, "\\.")}$`
+	`^[a-zA-Z0-9._%+-]+@${instituteDomain.replace(/\./g, "\\.")}$`
 );
 
 export const postFacultyBodySchema = z.object({
-  email: z.string().email({ message: "Please enter a valid email" }),
-  password: z
-    .string()
-    .min(6, { message: "Password should be at least 6 characters" }),
-  first_name: z.string({ required_error: "First name is required" }),
-  middle_name: z.string().optional(),
-  last_name: z.string().optional(),
-  phone: z
-    .string()
-    .length(10, { message: "Phone number must be exactly 10 digits" }),
-  designation: z.enum([
-    Designation.HOD,
-    Designation.LECTURER,
-    Designation.Tutor,
-  ]),
+	email: z
+		.string()
+		.email({ message: "Please enter a valid email" })
+		.regex(emailRegex, `email must be on ${instituteDomain} domain`),
+	password: z
+		.string()
+		.min(6, { message: "Password should be at least 6 characters" }),
+	first_name: z.string({ required_error: "First name is required" }),
+	middle_name: z.string().optional(),
+	last_name: z.string().optional(),
+	phone: z
+		.number()
+		.min(1000000000, { message: "phone number should be at least 10 digits" })
+		.max(9999999999, { message: "phone number should be at least 10 digits" }),
+	designation: z.enum([
+		Designation.HOD,
+		Designation.LECTURER,
+		Designation.Tutor,
+	]),
 });
 
 export type PostFacultyBody = z.infer<typeof postFacultyBodySchema>;
 
 export const putFacultyBodySchema = z.object({
-  email: z.string().email().optional(),
-  first_name: z.string().optional(),
-  middle_name: z.string().optional(),
-  last_name: z.string().optional(),
-  phone: z.string().length(10).optional(),
-  designation: z
-    .enum([Designation.HOD, Designation.LECTURER, Designation.Tutor])
-    .optional(),
+	email: z.string().email().optional(),
+	first_name: z.string().optional(),
+	middle_name: z.string().optional(),
+	last_name: z.string().optional(),
+	phone: z
+		.number()
+		.min(1000000000, { message: "phone number should be at least 10 digits" })
+		.max(9999999999, { message: "phone number should be at least 10 digits" })
+		.optional(),
+	designation: z
+		.enum([Designation.HOD, Designation.LECTURER, Designation.Tutor])
+		.optional(),
 });
 
 export type PutFacultyBody = z.infer<typeof putFacultyBodySchema>;
