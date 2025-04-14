538db3a47ba7d88e790e4d5b11c4b5a55c10ce44
kirti <kirti25032007@gmail.com>
2025-04-14 13:21:09 +0530

    feat:delete batch

--- src/modules/batch/batch.controller.ts
+++ src/modules/batch/batch.controller.ts
@@ -2,128 +2,163 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { PostBatchDetailsSchema, PostBatchSchema } from "./batch.schema";
 import {
-	createBatchService,
-	getAllBatchService,
-	getBatchService,
+  createBatchService,
+  getAllBatchService,
+  getBatchService,
 } from "./batch.service";
 import logger from "../../libs/logger";
 import { addSemesterDetailsAndSubjectsService } from "../semester/semester.service";
 
 export async function postBatchHandler(
-	req: Request<{}, {}, PostBatchSchema>,
-	res: Response
+  req: Request<{}, {}, PostBatchSchema>,
+  res: Response
 ) {
-	try {
-		const { branch_id, end_year, start_year, type } = req.body;
-		await createBatchService({
-			branch_id,
-			end_year,
-			start_year,
-			type,
-		});
+  try {
+    const { branch_id, end_year, start_year, type } = req.body;
+    await createBatchService({
+      branch_id,
+      end_year,
+      start_year,
+      type,
+    });
 
-		res.status(StatusCodes.CREATED).json({
-			message: "Batch created successfully",
-		});
+    res.status(StatusCodes.CREATED).json({
+      message: "Batch created successfully",
+    });
 
-		return;
-	} catch (err: any) {
-		if (err && err.code === "23503") {
-			if (err.constraint === "batch_branch_id_branch_id_fk") {
-				res.status(StatusCodes.BAD_REQUEST).json({
-					message: "Error creating batch",
-					errors: {
-						branch_id: "Branch doesn't exists",
-					},
-				});
+    return;
+  } catch (err: any) {
+    if (err && err.code === "23503") {
+      if (err.constraint === "batch_branch_id_branch_id_fk") {
+        res.status(StatusCodes.BAD_REQUEST).json({
+          message: "Error creating batch",
+          errors: {
+            branch_id: "Branch doesn't exists",
+          },
+        });
 
-				return;
-			}
-		}
+        return;
+      }
+    }
 
-		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-			message: "Internal server Error",
-		});
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: "Internal server Error",
+    });
 
-		return;
-	}
+    return;
+  }
 }
 
 export async function postBatchDetailsHandler(
-	req: Request<{ id: string }, {}, PostBatchDetailsSchema>,
-	res: Response
+  req: Request<{ id: string }, {}, PostBatchDetailsSchema>,
+  res: Response
 ) {
-	try {
-		const { id } = req.params;
-		const { semesters } = req.body;
+  try {
+    const { id } = req.params;
+    const { semesters } = req.body;
 
-		// check if details of all semesters is provided
-		// check it by using length
+    // check if details of all semesters is provided
+    // check it by using length
 
-		await addSemesterDetailsAndSubjectsService({
-			semesters,
-			batchId: +id,
-		});
+    await addSemesterDetailsAndSubjectsService({
+      semesters,
+      batchId: +id,
+    });
 
-		res.status(StatusCodes.OK).json({
-			message: "Semester details added successfully",
-		});
+    res.status(StatusCodes.OK).json({
+      message: "Semester details added successfully",
+    });
 
-		return;
-	} catch (err) {
-		console.error(err);
-		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-			message: "Internal server error",
-		});
+    return;
+  } catch (err) {
+    console.error(err);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: "Internal server error",
+    });
 
-		return;
-	}
+    return;
+  }
 }
 
 export async function getAllBatchHandler(req: Request, res: Response) {
-	try {
-		const results = await getAllBatchService();
+  try {
+    const results = await getAllBatchService();
 
-		res.status(StatusCodes.OK).json({
-			message: "Batch fetched successfully",
-			payload: results,
-		});
-		return;
-	} catch (err) {
-		console.error(err);
-		logger.error("Error getting all Batch", "BATCH");
+    res.status(StatusCodes.OK).json({
+      message: "Batch fetched successfully",
+      payload: results,
+    });
+    return;
+  } catch (err) {
+    console.error(err);
+    logger.error("Error getting all Batch", "BATCH");
 
-		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-			message: "Internal server error",
-		});
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: "Internal server error",
+    });
 
-		return;
-	}
+    return;
+  }
 }
 
 export async function getBatchHandler(
-	req: Request<{ id: number }>,
-	res: Response
+  req: Request<{ id: number }>,
+  res: Response
 ) {
-	try {
-		const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-		const batch = await getBatchService(id);
+    const batch = await getBatchService(id);
 
-		res.status(StatusCodes.OK).json({
-			message: "Batch fetched successfully",
-			payload: batch,
-		});
+    res.status(StatusCodes.OK).json({
+      message: "Batch fetched successfully",
+      payload: batch,
+    });
 
-		return;
-	} catch (err) {
-		console.error(err);
-		logger.error("Error getting single Batch", "BATCH");
+    return;
+  } catch (err) {
+    console.error(err);
+    logger.error("Error getting single Batch", "BATCH");
 
-		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-			message: "Internal server error",
-		});
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: "Internal server error",
+    });
 
-		return;
-	}
+    return;
+  }
+}
+
+export async function deleteBatchHandler(
+  req: Request<{ id: number }>,
+  res: Response
+) {
+  try {
+    const { id } = req.params;
+
+    // check if batch exists
+    const batch = await getBatchService(id);
+
+    if (!batch) {
+      res.status(StatusCodes.NOT_FOUND).json({
+        message: "Batch not found",
+      });
+
+      return;
+    }
+
+    res.status(StatusCodes.OK).json({
+      message: "Batch deleted successfully",
+    });
+
+    return;
+  } catch (err) {
+    console.error(err);
+    logger.error("Error deleting Batch", "BATCH");
+
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: "Internal server error",
+    });
+
+    return;
+  }
 }
--- src/modules/batch/batch.routes.ts
+++ src/modules/batch/batch.routes.ts
@@ -1,26 +1,33 @@
 import { Router } from "express";
 import { validateRequestBody } from "../../middlewares/validate-request.middleware";
 import { postBatchDetailsSchema, postBatchSchema } from "./batch.schema";
+import { requireRoleMiddleware } from "@/middlewares/require-role.middleware";
+import { Role } from "@/db/schema/user";
 import {
-	getAllBatchHandler,
-	getBatchHandler,
-	postBatchDetailsHandler,
-	postBatchHandler,
+  deleteBatchHandler,
+  getAllBatchHandler,
+  getBatchHandler,
+  postBatchDetailsHandler,
+  postBatchHandler,
 } from "./batch.controller";
 
 export const batchRouter = Router();
 
 batchRouter
-	.route("/")
-	.post(validateRequestBody(postBatchSchema), postBatchHandler)
-	.get(getAllBatchHandler);
+  .route("/")
+  .post(validateRequestBody(postBatchSchema), postBatchHandler)
+  .get(getAllBatchHandler);
 
 batchRouter.post(
-	"/:id/details",
-	validateRequestBody(postBatchDetailsSchema),
-	postBatchDetailsHandler
+  "/:id/details",
+  validateRequestBody(postBatchDetailsSchema),
+  postBatchDetailsHandler
 );
 
-batchRouter.route("/:id").get(getBatchHandler);
+batchRouter;
+batchRouter
+  .route("/:id")
+  .get(getBatchHandler)
+  .delete(requireRoleMiddleware(Role.ADMIN), deleteBatchHandler);
 
 export default batchRouter;
